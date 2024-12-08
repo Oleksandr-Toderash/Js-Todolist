e8929d3ad0941d6f18be9e3e7d1f3b00f2a610ed
Oleksandr Toderash <oleksandrtode@gmail.com>
2024-12-08 18:30:20 +0200

    Setting babel-loader, css-loader, HtmlWebpackPlagin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  module: {
+    rules: [
+      // babel-loader
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      // css-sass-loader
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: false,
+    liveReload: true,
+  },
 };
