d9bae23c2f1d197b595749de115af9e7b94e445a
Oleksandr Toderash <oleksandrtode@gmail.com>
2024-12-11 16:03:26 +0200

    Made the emptyTaskContainer function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@
 import "./components/css/general.css";
 import "./components/css/input-container.css";
 import "./components/css/todo-container.css";
+
+// js
+import "./components/js/todo-container.js"
