3052468f144af485c8104d51e32db6d0c0018c95
Oleksandr Toderash <oleksandrtode@gmail.com>
2024-12-12 14:00:00 +0200

    Add a function to create tasks and append them to the task list

--- src/components/js/todo-container.js
+++ src/components/js/todo-container.js
@@ -1,31 +1,68 @@
-const inputText = document.querySelector('.inputText').value;
+const inputTextValue = document.querySelector('.inputText').value;
+const inputText = document.querySelector('.inputText');
 const addBtn = document.querySelector('#addBtn');
 let taskList = document.querySelector('#taskList');
 
 class Task {
-  constructor(name, mark) {
+  constructor(name) {
     this.name = name;
-    this.mark = mark;
   }
 
   defineTask() {
     // create an element
+    const divClassElem = document.createElement('div');
+    divClassElem.classList.add('task-container');
+
+    const pNameClassElem = document.createElement('p');
+    pNameClassElem.style.paddingLeft = '3rem';
+    pNameClassElem.textContent = this.name;
+
+    const pMarkClassElem = document.createElement('p');
+    pMarkClassElem.classList.add('task-status');
+    pMarkClassElem.textContent = 'Pending';
+
+    const divIconClassElem = document.createElement('div');
+    divIconClassElem.classList.add('tasklist');
+
+
+    const imgClassElem = document.createElement('img');
+    imgClassElem.classList.add('delete-icon-box');
+    imgClassElem.src = 'https://cdn-icons-png.flaticon.com/512/5972/5972943.png'
+
+    divIconClassElem.append(imgClassElem)
+
+    divClassElem.appendChild(pNameClassElem)
+    divClassElem.appendChild(pMarkClassElem)
+    divClassElem.appendChild(divIconClassElem)
+
+    return divClassElem;
   }
 }
 
 function emptyTaskContainer() {
+  document.querySelector('#emptyTaskContainer').style.display = 'none';
+}
+
+function newTask() {
   addBtn.addEventListener('click', () => {
-    if (taskList.children.length > 0) {
-      document.querySelector('#emptyTaskContainer').style.display = 'none';
-    } else if (taskList.children.length <= 0) {
-      /* no-op */
+    // create a task
+    if (taskList.children.length >= 0) {
+      const newTask = new Task(inputText.value).defineTask();
+      taskList.appendChild(newTask);
+      emptyTaskContainer()
     }
+
+    inputText.value = ''
   })
+
+  if (taskList.children.length > 0) {
+    emptyTaskContainer();
+  }
 }
+
 function createTask() {
-  emptyTaskContainer();
+  newTask();
 }
-createTask()
-console.log(taskList.children.length)
-// In this element todo-container, make an if else statement. if the element todo-container has 5 values inside, remove the emptyTaskContainer, if value 4, add.
+
+createTask();
 
